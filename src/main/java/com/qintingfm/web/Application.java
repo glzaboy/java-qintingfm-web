8918d3814ab84496ebf576d923d8a85450330441
glzaboy <glzaboy@163.com>
2019-11-24 02:53:52 +0800

    初始化程序

--- src/main/java/com/qintingfm/web/Application.java
+++ src/main/java/com/qintingfm/web/Application.java
@@ -1,13 +1,15 @@
-package com.example.demo;
+package com.qintingfm.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableScheduling
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
