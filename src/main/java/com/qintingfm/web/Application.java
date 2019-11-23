84f667cb1331d470127eb7b29496e1cf9b8ab64c
glzaboy <glzaboy@163.com>
2019-11-24 03:02:02 +0800

    增加async支持

--- src/main/java/com/qintingfm/web/Application.java
+++ src/main/java/com/qintingfm/web/Application.java
@@ -1,11 +1,14 @@
 package com.qintingfm.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
