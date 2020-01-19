d07ffd53b318f8c3c9f154c27f0dc9f2b05de2d7
guliuzhong <glzaboy@163.com>
2020-01-19 11:32:59 +0800

    优化端口和和url获取 ，增加xmlrpc的信息页。

--- src/main/java/com/qintingfm/web/controller/XmlRpc.java
+++ src/main/java/com/qintingfm/web/controller/XmlRpc.java
@@ -1,14 +1,13 @@
 package com.qintingfm.web.controller;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -16,7 +15,7 @@ import java.io.IOException;
 @RequestMapping("/xmlrpc")
 @Slf4j
 public class XmlRpc {
-    @RequestMapping(value = "/xmlrpcserver" ,method = {RequestMethod.POST,RequestMethod.GET,RequestMethod.OPTIONS})
+    @RequestMapping(value = "/xmlrpcserver" ,method = {RequestMethod.POST,RequestMethod.OPTIONS},produces = {"application/xml;charset=utf-8"})
     @ResponseBody
     public String xmlRpcServer(@Autowired HttpServletRequest  request) throws IOException {
         ServletInputStream inputStream = request.getInputStream();
@@ -33,5 +32,20 @@ public class XmlRpc {
 
         log.info(byteArrayOutputStream.toString());
         return "xmlrpc";
+    }
+    @RequestMapping(value = "/xmlrpcserver" ,method = {RequestMethod.GET},produces = {"application/xml;charset=utf-8"})
+    @ResponseBody
+    public String xmlRpcServer( ) {
+        return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "<rsd version=\"1.0\" xmlns=\"http://archipelago.phrasewise.com/rsd\">\n" +
+                "\t<service>\n" +
+                "\t\t<engineName>MetaWeblog</engineName>\n" +
+                "\t\t<engineLink>http://qintingfm.com/</engineLink>\n" +
+                "\t\t<homePageLink>http://qintingfm.com/</homePageLink>\n" +
+                "\t\t<apis>\n" +
+                "\t\t\t<api name=\"MetaWeblog\" blogID=\"1\" preferred=\"false\" apiLink=\""+ ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString() +"\" />\n" +
+                "\t\t</apis>\n" +
+                "\t</service>\n" +
+                "</rsd>";
     }
 }
