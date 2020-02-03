5284d5902bffb93f4c8c47649ae143aed9c27462
guliuzhong <glzaboy@163.com>
2020-02-04 00:41:09 +0800

    优化一处保存出错。

--- src/main/java/com/qintingfm/web/jpa/entity/Blog.java
+++ src/main/java/com/qintingfm/web/jpa/entity/Blog.java
@@ -19,11 +19,11 @@ public class Blog implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "genBlogid")
     private Integer postId;
     String title;
-    @OneToOne(cascade = CascadeType.PERSIST,fetch = FetchType.LAZY)
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     @JoinColumn(name = "contId",nullable = false)
     private BlogCont blogCont;
     private String shotCont;
-    @OneToMany(cascade = CascadeType.PERSIST,fetch = FetchType.LAZY)
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     @JoinTable(name = "qt_blog2cat",joinColumns = {@JoinColumn(name = "postId")},inverseJoinColumns = {@JoinColumn(name = "catId")})
     private Collection<Category> blogCategory;
     private Date dateCreated;
