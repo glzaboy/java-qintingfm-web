c6135590ac8e51df143fc0730cf17a8cb0bc692a
guliuzhong <glzaboy@163.com>
2020-02-05 17:05:26 +0800

    增加不验证的方法，修改主题

--- src/main/java/com/qintingfm/web/config/WebSecurityConfig.java
+++ src/main/java/com/qintingfm/web/config/WebSecurityConfig.java
@@ -60,7 +60,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/user/login", "/xmlrpc/server","/misc/changeTheme","/xmlrpc.php","/","/page/*","/blog/**").permitAll()
-                .antMatchers(HttpMethod.POST,"/xmlrpc/xmlrpcserver").permitAll()
                 .and().authorizeRequests().anyRequest().authenticated()
                 .and().formLogin()
                 .loginPage("/user/login").loginProcessingUrl("/login").
