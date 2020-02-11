9621251918cae2405be11fa2972622fa656a4818
guliuzhong <glzaboy@163.com>
2020-02-11 15:38:47 +0800

    除除spring-boot-starter-data-jdbc

--- src/main/java/com/qintingfm/web/Application.java
+++ src/main/java/com/qintingfm/web/Application.java
@@ -1,9 +1,7 @@
 package com.qintingfm.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,7 +17,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAsync
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "com.qintingfm.web.jpa")
-@EnableJdbcRepositories(basePackages = "com.qintingfm.web.jdbc")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
