01a14412c6ce621217960e481ae10aeba5ec2894
guliuzhong <glzaboy@163.com>
2020-02-23 04:42:08 +0800

    修正一处 BUG.

--- src/main/java/com/qintingfm/web/service/BlogService.java
+++ src/main/java/com/qintingfm/web/service/BlogService.java
@@ -81,8 +81,8 @@ public class BlogService {
         Collection<String> pushUrl = new ArrayList<>();
         Blog save = blogJpa.save(blog);
         try {
-            Method detail = BlogController.class.getDeclaredMethod("detail", ModelAndView.class, Integer.class);
-            String s = MvcUriComponentsBuilder.fromMethod(BlogController.class, detail, null, Integer.valueOf(save.getPostId())).build().toString();
+            Method detail = BlogController.class.getDeclaredMethod("detail", ModelAndView.class, Integer.class,Integer.class);
+            String s = MvcUriComponentsBuilder.fromMethod(BlogController.class, detail, null, Integer.valueOf(save.getPostId()),null).build().toString();
             pushUrl.add(s);
             baiduSpider.pushUrlToSpider(pushUrl);
         } catch (NoSuchMethodException e) {
